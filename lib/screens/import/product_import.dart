bdaad6c8987bb63a639b52f8b373c3f4755c493d
Eanda <eandalined@gmail.com>
2021-05-12 16:45:17 +0700

    import

--- lib/screens/import/product_import.dart
+++ lib/screens/import/product_import.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class ProductImportBody extends StatefulWidget {
   const ProductImportBody({Key key}) : super(key: key);
@@ -8,13 +11,35 @@ class ProductImportBody extends StatefulWidget {
 }
 
 class _ProductImportBodyState extends State<ProductImportBody> {
+
+  List<dynamic> vData = [];
+
+  @override
+  void initState() {
+    this._fetchItems();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: const Color(0xffeeee00),
-      height: 800.0,
-      alignment: Alignment.center,
-      child: const Text('Fixed Height Content'),
+      color: Color(0xffeeee00),
+      child: _buildBody()
     );
+  }
+
+  Widget _buildBody() {
+    return ListView.separated(
+      itemCount: vData.length,
+    );
+  }
+
+  _fetchItems() async {
+    final data = await rootBundle.loadString('assets/json_data/product_imports.json');
+    Map mapItems = jsonDecode(data);
+    setState(() {
+      this.vData = mapItems['imports'];
+    });
+    return this.vData;
   }
 }

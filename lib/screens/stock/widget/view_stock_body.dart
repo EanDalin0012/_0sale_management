acb91c9e6bab8b6af5543f265ad7f0e86eab60c9
Eanda <eandalined@gmail.com>
2021-05-16 14:50:26 +0700

    view stock

--- lib/screens/stock/widget/view_stock_body.dart
+++ lib/screens/stock/widget/view_stock_body.dart
@@ -1,11 +1,16 @@
+import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:sale_management/screens/constants.dart';
 import 'package:sale_management/screens/size_config.dart';
+import 'package:sale_management/screens/widgets/circular_progress_indicator/circular_progress_loading.dart';
 import 'package:sale_management/share/constant/constant_color.dart';
 import 'package:sale_management/share/constant/text_style.dart';
 import 'package:sale_management/share/model/key/m_key.dart';
+import 'package:sale_management/share/model/key/stock_detail_key.dart';
+import 'package:sale_management/share/utils/number_format.dart';
 
 class ViewStockBody extends StatefulWidget {
   final Map vData;
@@ -16,7 +21,14 @@ class ViewStockBody extends StatefulWidget {
 }
 
 class _ViewStockBodyState extends State<ViewStockBody> {
-  String _place = "country";
+
+  List<dynamic> vData = [];
+
+  @override
+  void initState() {
+    this._fetchItems();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,39 +41,43 @@ class _ViewStockBodyState extends State<ViewStockBody> {
               children: <Widget>[
                 SizedBox(height: SizeConfig.screenHeight * 0.04), // 4%
                 Text("Stock", style: headingStyle),
-                // Text(
-                //   "Complete your details.",
-                //   textAlign: TextAlign.center,
-                // ),
+                Text(
+                  "Details stock of product.",
+                  textAlign: TextAlign.center,
+                ),
               ],
             ),
           ),
           _buildStockName(context),
-          _buildCardSell(),
-          _buildCardInStock()
-          // _placeNav(context),
+          this.vData.length > 0 ? Column(
+            children: this.vData.map((item) {
+              return _buildProductCard(item);
+            }).toList(),
+          ) : CircularProgressLoading(),
+          // _buildProductCard(),
+          // _buildProductCard(),
         ],
       ),
     );
   }
 
-  Widget _buildCardSell() {
+  Widget _buildCardSell(Map item) {
     return Container(
         height: 150.0,
         width: MediaQuery.of(context).size.width,
-        margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        //margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
         padding: EdgeInsets.symmetric(horizontal: 5.0, vertical: 5.0),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(30.0),
           color: Color(0XFFAEA1E5).withOpacity(0.3),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 3,
-              blurRadius: 7,
-              offset: Offset(0, 2), // changes position of shadow
-            ),
-          ],
+          // boxShadow: [
+          //   BoxShadow(
+          //     color: Colors.grey.withOpacity(0.5),
+          //     spreadRadius: 3,
+          //     blurRadius: 7,
+          //     offset: Offset(0, 2), // changes position of shadow
+          //   ),
+          // ],
         ),
         child: Container(
           width: 160.0,
@@ -93,14 +109,14 @@ class _ViewStockBodyState extends State<ViewStockBody> {
                   ],
                 ),
                 Text(
-                  "Quantity: 100",
+                  "Quantity: "+item[StockDetailsKey.quantity].toString(),
                   style: TextStyle(color: dropColor, fontSize: 15.0),
                 ),
                 SizedBox(
                   height: 5,
                 ),
                 Text(
-                  "Total: 1000.00 USD",
+                  "Total: " + FormatNumber.usdFormat2Digit(item[StockDetailsKey.total].toString()) + " USD",
                   style: TextStyle(color: dropColor, fontSize: 15.0),
                 ),
               ],
@@ -110,23 +126,66 @@ class _ViewStockBodyState extends State<ViewStockBody> {
     );
   }
 
-  Widget _buildCardInStock() {
+  Widget _buildProductCard(Map data) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 330,
+      margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+      padding: EdgeInsets.symmetric(horizontal: 5.0, vertical: 5.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30.0),
+        color: Color(0XFFAEA1E5).withOpacity(0.2),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 1,
+            blurRadius: 7,
+            offset: Offset(0, 2), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Container(
+        width: 160.0,
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(25.0)
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(data[StockDetailsKey.productName],
+                style: TextStyle( color: dropColor, fontSize: 20.0, fontFamily: fontFamilyDefault)
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            _buildCardSell(data[StockDetailsKey.sell]),
+            _buildCardInStock(data[StockDetailsKey.inStock])
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCardInStock(Map item) {
     return Container(
         height: 110.0,
         width: MediaQuery.of(context).size.width,
-        margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+        //margin: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
         padding: EdgeInsets.symmetric(horizontal: 5.0, vertical: 5.0),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(30.0),
           color: Color(0XFFAEA1E5).withOpacity(0.3),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 3,
-              blurRadius: 7,
-              offset: Offset(0, 2), // changes position of shadow
-            ),
-          ],
+          // boxShadow: [
+          //   BoxShadow(
+          //     color: Colors.grey.withOpacity(0.5),
+          //     spreadRadius: 3,
+          //     blurRadius: 7,
+          //     offset: Offset(0, 2), // changes position of shadow
+          //   ),
+          // ],
         ),
         child: Container(
           width: 160.0,
@@ -141,20 +200,13 @@ class _ViewStockBodyState extends State<ViewStockBody> {
               mainAxisAlignment: MainAxisAlignment.start,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text(
-                      "In Stock",
-                      style: TextStyle(color: Colors.white, fontSize: 25.0, fontFamily: fontFamilyDefault),
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.more_vert, color: Colors.white,),
-                    )
-                  ],
-                ),
                 Text(
-                  "Quantity: 100",
+                  "In Stock",
+                  style: TextStyle(color: Colors.white, fontSize: 25.0, fontFamily: fontFamilyDefault),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "Quantity: "+item[StockDetailsKey.quantity].toString(),
                   style: TextStyle(color: dropColor, fontSize: 15.0),
                 ),
                 SizedBox(
@@ -255,5 +307,15 @@ class _ViewStockBodyState extends State<ViewStockBody> {
       // }
     },
   );
+
+  _fetchItems() async {
+    final data = await rootBundle.loadString('assets/json_data/show_stock_by_product.json');
+    Map mapItems = jsonDecode(data);
+    setState(() {
+      this.vData = mapItems['stocks'];
+      print('${this.vData}');
+    });
+    return this.vData;
+  }
 
 }
--- lib/screens/widgets/side_nav.dart
+++ lib/screens/widgets/side_nav.dart
@@ -13,152 +13,99 @@ class SideNave extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     size = MediaQuery.of(context).size;
-    print('${size.height}');
     return Drawer(
             child: SafeArea(
               top: false,
               child: Scaffold(
-                body: SingleChildScrollView(
-                  child: Column(
-                      children: <Widget>[
-                        Container(
-                          padding: EdgeInsets.only(top: 15, left: 8, right: 8, bottom: 15),
-                          color: Color(0xFF88070B),
-                          child:Row(
-                            children: <Widget>[
-                              ClipRRect(
-                                borderRadius: BorderRadius.circular(100.0),
-                                child: Image.asset("assets/images/profile.jpg", width: 80, height: 80, fit: BoxFit.fill,),
-                              ),
-                              SizedBox(width: 15,),
-                              RichText(
-                                  text: TextSpan(
-                                      children: [
-                                        TextSpan(text: "Name Surname\n", style: TextStyle(fontWeight: FontWeight.bold, fontFamily: fontFamilyDefault, color: Colors.white)),
-                                        TextSpan(text: "@username", style: TextStyle(fontFamily: fontFamilyDefault, color: Colors.white)),
-                                      ]
-                                  )
-                              )
-                            ],
-                          )
-                        ),
-                        ListTile(title: Text("Contact Us", style: style,), leading: Icon(Icons.phone),),
-                        ListTile(title: Text("Terms & Condition", style: style), leading: Icon(Icons.card_giftcard),),
-                        Divider(),
-                        ListTile(
-                          onTap: () {
-                            Navigator.pop(context);
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => SettingScreen()),
-                            );
-                          },
-                          title: Text("Settings",style: style), leading: Icon(Icons.settings),),
-                        // ListTile(title: Text("About"), leading: Icon(Icons.info),),
-                        // ListTile(title: Text("Logout"), leading: Icon(Icons.exit_to_app),),
-                        Column(
+                body: Stack(
+                  children: <Widget>[
+                    SingleChildScrollView(
+                      child: Container(
+                        height: size.height,
+                        width: size.width,
+                        child: Column(
                           children: <Widget>[
+                            Container(
+                                padding: EdgeInsets.only(top: 15, left: 8, right: 8, bottom: 15),
+                                color: Color(0xFF88070B),
+                                child:Row(
+                                  children: <Widget>[
+                                    ClipRRect(
+                                      borderRadius: BorderRadius.circular(100.0),
+                                      child: Image.asset("assets/images/profile.jpg", width: 80, height: 80, fit: BoxFit.fill,),
+                                    ),
+                                    SizedBox(width: 15,),
+                                    RichText(
+                                        text: TextSpan(
+                                            children: [
+                                              TextSpan(text: "Name Surname\n", style: TextStyle(fontWeight: FontWeight.bold, fontFamily: fontFamilyDefault, color: Colors.white)),
+                                              TextSpan(text: "@username", style: TextStyle(fontFamily: fontFamilyDefault, color: Colors.white)),
+                                            ]
+                                        )
+                                    )
+                                  ],
+                                )
+                            ),
+                            ListTile(title: Text("Contact Us", style: style,), leading: Icon(Icons.phone),),
+                            ListTile(title: Text("Terms & Condition", style: style), leading: Icon(Icons.card_giftcard),),
+                            Divider(),
                             ListTile(
-                              title: new Text(
-                                "Logout",
-                                style: new TextStyle(
-                                  // fontFamily: AppTheme.fontName,
-                                  fontWeight: FontWeight.w600,
-                                  fontSize: 16,
-                                  // color: AppTheme.darkText,
-                                ),
-                                textAlign: TextAlign.left,
-                              ),
-                              trailing: new Icon(
-                                Icons.power_settings_new,
-                                color: Colors.red,
-                              ),
                               onTap: () {
                                 Navigator.pop(context);
                                 Navigator.push(
                                   context,
-                                  MaterialPageRoute(builder: (context) => SignInScreen()),
+                                  MaterialPageRoute(builder: (context) => SettingScreen()),
                                 );
                               },
+                              title: Text("Settings",style: style), leading: Icon(Icons.settings),
                             ),
-                            SizedBox(
-                              height: MediaQuery.of(context).padding.bottom,
-                            )
+                            Column(
+                              children: <Widget>[
+                                ListTile(
+                                  title: new Text(
+                                    "Logout",
+                                    style: new TextStyle(
+                                      // fontFamily: AppTheme.fontName,
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 16,
+                                      // color: AppTheme.darkText,
+                                    ),
+                                    textAlign: TextAlign.left,
+                                  ),
+                                  trailing: new Icon(
+                                    Icons.power_settings_new,
+                                    color: Colors.red,
+                                  ),
+                                  onTap: () {
+                                    Navigator.pop(context);
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(builder: (context) => SignInScreen()),
+                                    );
+                                  },
+                                ),
+                                SizedBox(
+                                  height: MediaQuery.of(context).padding.bottom,
+                                )
+                              ],
+                            ),
                           ],
                         ),
-                        Container(
-                          width: double.infinity,
-                          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 60.0,),
-                          color: Colors.grey.shade200,
-                          child: Text("2.20.00"),
-                        )
-                      ]
-                  ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      child: Container(
+                        width: size.width,
+                        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 60.0,),
+                        color: Color(0XFFAEA1E5).withOpacity(0.3),
+                        child: Text("2.20.00"),
+                      )
+                    )
+                  ],
                 ),
               ),
             )
-    );
-  }
-
-  Container buildUserInfo(context) => Container(
-    color: Color(0xFF88070B),
-    // height: MediaQuery.of(context).size.height * 0.3,
-    width: MediaQuery.of(context).size.height,
-    padding: EdgeInsets.only(bottom: 25.0),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      mainAxisSize: MainAxisSize.min,
-      children: <Widget>[
-        _buildProfile(
-          color: Colors.white,
-          height: 70.0,
-          width: 70.0,
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-                Text(
-                  'Good Afternoon!',
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.w300,
-                    color: Colors.white,
-                  ),
-                ),
-                SizedBox(
-                  width: 5,
-                ),
-                Text(
-                  'MAUSAM'.toUpperCase(),
-                  style: TextStyle(
-                    fontSize: 17.0,
-                    fontWeight: FontWeight.w300,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-        ),
-
-      ],
-    ),
-  );
-
-  Container _buildProfile({double height, width, Color color}) {
-    return Container(
-      width: width,
-      height: height,
-        decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: Colors.deepPurpleAccent,
-            image: DecorationImage(
-              image: AssetImage('assets/images/profile.jpg'),
-              fit: BoxFit.contain,
-            ),
-          border: Border.all(
-            color: color,
-            width: 3.0,
-          ),
-        )
     );
   }
 }
